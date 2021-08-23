bc52b6b0786036583f2dfb2236712c4e24372f9a
vladikuss <vreshetoff@gmail.com>
2021-08-23 17:52:20 +0500

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-
 exports.min = function min (array) {
-  return 0;
-  
+    if (typeof array != "undefined" && array != null && array.length != null && array.length > 0) {
+        return array.reduce(function (p, v) {
+            return ( p < v ? p : v );
+          });
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array != "undefined" && array != null && array.length != null && array.length > 0) {
+        return array.reduce(function (p, v) {
+            return ( p > v ? p : v );
+          });
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array != "undefined" && array != null && array.length != null && array.length > 0) {
+        return array.reduce((a, b) => (a + b)) / array.length;
+    } else {
+        return 0;
+    }
 }
